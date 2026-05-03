1b31fd58fe4ffd9098c0f2804b622232002c7b1c
Your Name <23096yb@hvhs.school.nz>
2026-05-04 10:59:31 +1200

    mr.tom salaway year 12 physiscscs

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // The ref('/') part tells the operation to write to the base level of the database "/"
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
-function helloWorld(){
+function helloWorld() {
   console.log("Running helloWorld()")
   firebase.database().ref('/').set(
     {
@@ -25,7 +25,7 @@ function helloWorld(){
     }
   )
 }
-function byeWorld(){
+function byeWorld() {
   console.log("Running byeWorld()")
   firebase.database().ref('/').set(
     {
@@ -34,51 +34,52 @@ function byeWorld(){
   )
 }
 function simpleRead() {
-    console.log("Reading message");
-    firebase.database().ref('/').child('message').once('value', displayRead, fb_readError);
-    console.log("Leaving simpleRead")
+  console.log("Reading message");
+  firebase.database().ref('/').child('message').once('value', displayRead, fb_readError);
+  console.log("Leaving simpleRead")
 }
 function displayRead(snapshot) {
-    console.log("Running displayRead(), the message is: " + snapshot.val())
-    HTML_OUTPUT.innerHTML = snapshot.val();
+  console.log("Running displayRead(), the message is: " + snapshot.val())
+  HTML_OUTPUT.innerHTML = snapshot.val();
 }
 function display(snapshot) {
-    var dbData = snapshot.val();
-    if (dbData == null) { // if there is no data, dbData will be null.
-        console.log('There was no record when trying to read the message');
-    }
-    else {
-        console.log("The message is: " + dbData)
-    }
+  var dbData = snapshot.val();
+  if (dbData == null) { // if there is no data, dbData will be null.
+    console.log('There was no record when trying to read the message');
+  }
+  else {
+    console.log("The message is: " + dbData)
+  }
 }
 function fb_readListener() {
   console.log("Read Listener");
   firebase.database().ref('/message').on('value', displayRead)
 }
-function zipyDipy(){
+function zipyDipy() {
   console.log("Running zipyDipy()")
-  firebase.database().ref('/').set()
-   highScoreTable = {
-      users: {
-        size: {
+  firebase.database().ref('/').set(
+    {
+      game1: {
+        users: {
           bigBoy: 67,
           smallBoy: 6.7,
           fillip: 12,
           bung: 8739457389,
         }
       },
-      users: {
-      elasticity: {
-        bigBoy: 67,
-        smallBoy: 6.7,
-        fillip: 12,
-        bung: 8739457389,
+      game2: {
+        users: {
+          bigBoy: 67,
+          smallBoy: 6.7,
+          fillip: 12,
+          bung: 8739457389,
+        }
       }
     }
+  )
 }
 firebase.database().ref('/').set(highScoreTable)
-}
-function fb_readHighScores(){
+function fb_readHighScores() {
   console.log("Reading high scores");
   firebase.database().ref('/highScores/game1').once('value', displayRead, fb_readError)
 }
